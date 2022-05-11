1cb6138695d14eac3024f353766705bc561b1b69
Jacobwj04 <32894@ma-web.nl>
2022-05-11 12:06:31 +0200

    popup

---
+++ main.js
@@ -0,0 +1,16 @@
+function showModal(name) {
+    var modalname = name;
+    modalname.style.display = "block";
+  
+    window.onclick = function(event) {
+      if (event.target == modalname) {
+        modalname.style.display = "none";
+      }
+    };
+  }
+
+function closeModal(name) {
+    // When the user clicks on <span> (x), close the modal
+    var modalname = name;
+    modalname.style.display = "none";
+};
